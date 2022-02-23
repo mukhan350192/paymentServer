9115c31dd5366ef0da044f9d1f70079097c94aa6
mukhan350192 <mukhan350192@gmail.com>
2022-02-23 18:34:07 +0600

    update

--- app/Http/Controllers/NashController.php
+++ app/Http/Controllers/NashController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class NashController extends Controller
 {
